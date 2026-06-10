bf7915b1b3829dc0c6165454ea92e87e211978ea
ABVicky <vickyprasadmahato34@gmail.com>
2026-06-10 15:50:16 +0530

    style: reduce global UI/UX sizes, sidebars, buttons, and input paddings

--- digispire-app/src/pages/admin/RevisionAppealsPage.jsx
+++ digispire-app/src/pages/admin/RevisionAppealsPage.jsx
@@ -151,7 +151,7 @@ export default function RevisionAppealsPage() {
         </div>
 
         {/* Search */}
-        <div className="relative w-full md:w-72">
+        <div className="relative w-full md:w-64">
           <Search size={14} className="absolute left-4 top-1/2 -translate-y-1/2 text-slate-400" />
           <input
             type="text"
